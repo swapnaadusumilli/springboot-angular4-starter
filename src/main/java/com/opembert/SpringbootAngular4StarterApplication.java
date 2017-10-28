e7bc9cbce61405fac9cc88f951e9e56354201dc5
O'Marie J. Pemberton <omarie_j_pemberton1@homedepot.com>
2017-10-28 15:15:48 -0400

    init

---
+++ src/main/java/com/opembert/SpringbootAngular4StarterApplication.java
@@ -0,0 +1,12 @@
+package com.opembert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngular4StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngular4StarterApplication.class, args);
+	}
+}
